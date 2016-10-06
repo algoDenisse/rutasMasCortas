cdff7875e16e05f9d08208fffe1b7173a31bf41a
Denisse <denissepaolarojas@hotmail.com>
2016-10-05 23:03:10 -0600

    Crea botones de entrada y esqueleto de tabla final. Leer de archivos WIP

--- src/series_deportivas.c
+++ src/series_deportivas.c
@@ -3,17 +3,18 @@
 #include <stdbool.h>
 
 GtkWidget *number_games, *ph_entry, *pr_entry, *partida, *warning_window;
-GtkBuilder      *initial_series_deportivas_table_builder;
-GtkWidget       *initial_series_deportivas_table_window;
-GtkWidget       *initial_series_deportivas_table;
+GtkBuilder      *series_deportivas_table_builder;
+GtkWidget       *series_deportivas_table_window;
+GtkWidget       *series_deportivas_table;
 
 GtkBuilder      *file_saver_builder;
 GtkWidget       *file_saver_window;
 GtkFileChooser *file_chooser;
 
-GtkWidget ***entrada;
+GtkWidget **entrada;
 
-int ph,pr,charPos,number_of_games,games_quantity, button_quantity=0;
+int ph,pr,charPos,number_of_games, defining_games_quantity = 0;
+int *global_numberofgames;
 
 double ph_double;
 double qr_double;
@@ -69,68 +70,131 @@ void create_warning_window(gchar* pMessage){
   gtk_widget_show_all(warning_window);
   gtk_main();
 }
-void create_entry_window(){
+
+void update_global_numberofgames(GtkWidget *widget, gpointer   data){
+	int val = data;
+
+	if(global_numberofgames[val] ==0){
+			global_numberofgames[val] = 1;
+			printf("HOLAAAA %d VAl = %d\n", data, global_numberofgames[val] );
+	}
+	else{
+		global_numberofgames[val] = 0;
+		printf("HOLAAAA %d VAl = %d\n", data, global_numberofgames[val] );
+	}
+}
+
+void solve_series_deportivas_problem(){
+	char game_number[25];
+	series_deportivas_table_builder= gtk_builder_new();
+  gtk_builder_add_from_file (series_deportivas_table_builder, "glade/initial_series_deportivas_table_window.glade", NULL);
+
+  series_deportivas_table_window = GTK_WIDGET(gtk_builder_get_object(series_deportivas_table_builder, "initial_series_deportivas_table_window"));
+  gtk_builder_connect_signals(series_deportivas_table_builder, NULL);
+
+  series_deportivas_table= gtk_grid_new();
+  gtk_grid_set_row_spacing (GTK_GRID (series_deportivas_table), 2);
+  gtk_container_add (GTK_CONTAINER (series_deportivas_table_window), series_deportivas_table);
+
+
+  GtkWidget ***entrada;
+	int j,k,i;
+  entrada=calloc(defining_games_quantity+2,sizeof(GtkWidget**));
+  for(j = 0; j < defining_games_quantity+2; j++){
+    entrada[j]=calloc(defining_games_quantity+2,sizeof(GtkWidget*));
+  }
+  for(k =0; k< defining_games_quantity+2;k++){
+    for(j=0;j<defining_games_quantity+2;j++){
+
+      entrada[k][j]= gtk_entry_new();
+      gtk_grid_attach (GTK_GRID (series_deportivas_table),entrada[k][j] , k, j, 1, 1);
+			gtk_widget_set_sensitive (entrada[k][j], FALSE);
+      if(k== 0 && j !=0){
+			snprintf(game_number,25,"%d",j-1);
+      gtk_entry_set_text (entrada[k][j],game_number);
+			gtk_widget_set_name(entrada[k][j], "column_name");
+      }
+      if (k != 0 && j == 0){
+				snprintf(game_number,25,"%d",k-1);
+	      gtk_entry_set_text (entrada[k][j],game_number);
+				gtk_widget_set_name(entrada[k][j], "column_name");
+
+      }
+    }
+  }
+
+	g_object_unref(series_deportivas_table_builder);
+
+	gtk_widget_show_all(series_deportivas_table_window);
+}
+
+void create_buttons_window(){
 	int k,j;
-	GtkWidget       *button;
-	GtkWidget       *button_box;
-	initial_series_deportivas_table_builder= gtk_builder_new();
-	gtk_builder_add_from_file (initial_series_deportivas_table_builder, "glade/initial_series_deportivas_table_window.glade", NULL);
+	char game_number[25];
+	GtkWidget *window;
+	GtkWidget *scrolledwindow;
+	GtkAdjustment *adjustmentValue;
+	GtkWidget *table;
+	GtkWidget *button;
+	GtkWidget *button_box;
 
-	initial_series_deportivas_table_window = GTK_WIDGET(gtk_builder_get_object(initial_series_deportivas_table_builder, "initial_series_deportivas_table_window"));
-	gtk_builder_connect_signals(initial_series_deportivas_table_builder, NULL);
+	/* create a new window */
+	 window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	 gtk_window_set_title (GTK_WINDOW (window),"Series Deportivas");
 
-	initial_series_deportivas_table= gtk_grid_new();
-	gtk_grid_set_row_spacing (GTK_GRID (initial_series_deportivas_table), 2);
-	gtk_container_add (GTK_CONTAINER (initial_series_deportivas_table_window), initial_series_deportivas_table);
+	 /*Scrolled window*/
 
+	 scrolledwindow = gtk_scrolled_window_new(NULL,NULL);
+	 gtk_widget_set_size_request(scrolledwindow, 432, 100);
+
+
+	 gtk_container_add (GTK_CONTAINER (window), scrolledwindow);
+
+	 // create new table
+	 table= gtk_grid_new();
+	 gtk_grid_set_row_spacing (GTK_GRID (table), 2);
+	 gtk_container_add (GTK_CONTAINER (scrolledwindow), table);
 	//dynamic grid
-	entrada=calloc(button_quantity+1,sizeof(GtkWidget**));
-	for(j = 0; j < button_quantity+1; j++  ){
-		entrada[j]=calloc(button_quantity+1,sizeof(GtkWidget*));
-	}
+	entrada=calloc(number_of_games+1,sizeof(GtkWidget*));
+	  int fila = 0;
+  for(j = 0; j < number_of_games; j++){
+		snprintf(game_number,25,"%d",j+1);
+    entrada[j]= gtk_toggle_button_new_with_label (game_number);
+		gtk_widget_set_name(entrada[j], "toggle_btn");
+		g_signal_connect (entrada[j], "toggled", G_CALLBACK (update_global_numberofgames),(gpointer) j);
 
-	//loops to create grid
-	for(j=0;j<button_quantity+1;j++){ //filas
-		for(k =0; k< 3;k++){ //columnas
-			entrada[j][k]= gtk_toggle_button_new ();
-			gtk_grid_attach (GTK_GRID (initial_series_deportivas_table),entrada[j][k] , k, j, 1, 1);
-			//printf("Puse la pos[%d][%d]\n",j,k );
-      // if((k==0) && (j!=0)){
-      //   snprintf(intermediate_buffer,5,"%d",j);
-      //   gtk_entry_set_text (entrada[j][k],intermediate_buffer);
-      //   gtk_widget_set_sensitive (entrada[j][k], FALSE);
-      // }
+		gtk_grid_attach (GTK_GRID (table),entrada[j], j, 1, 1, 1);
+  }
 
-		}
-	}
+	button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+	gtk_grid_attach (GTK_GRID (table),button_box ,(j/2)-1, j, 3, 2);
 
+	button = gtk_button_new_with_label ("Procesar");
+	g_signal_connect (button, "clicked", G_CALLBACK (solve_series_deportivas_problem), NULL);
+	gtk_container_add (GTK_CONTAINER (button_box), button);
 
-
-	// button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-	// gtk_grid_attach (GTK_GRID (initial_equipment_table),button_box ,1, j, 2, 2);
-  //
-	// button = gtk_button_new_with_label ("Procesar");
-	// g_signal_connect (button, "clicked", G_CALLBACK (solve_requipmentEquipment_problem), (gpointer) initial_equipment_table);
-	// gtk_container_add (GTK_CONTAINER (button_box), button);
-
-	g_object_unref(initial_series_deportivas_table_builder);
-
-	gtk_widget_show_all(initial_series_deportivas_table_window);
-//}
-
+		gtk_widget_show_all(window);
 }
 
 void on_load_file_button_file_set(){
   FILE *file;
   gchar *filename;
-  int k, in_char;
+  int i,k, in_char, ind = 0;
+
+	char **matriz_datos_iniciales = calloc(3, 500*sizeof(char));
+	//alojamos la memoria para cada espacio del char
+	for (i = 0; i < 3; ++i) {
+		 matriz_datos_iniciales[i] = (char *)malloc(500);
+	}
+
   filename=gtk_file_chooser_get_filename (file_chooser);
   file = fopen( filename, "r" );
     if(file){
       clear_token_buffer();
       while ((in_char = getc(file)) != EOF){
           if((in_char == '|')|| (in_char == '\n')){
-             printf("String buffer %s\n", string_buffer );
+						 strcpy(matriz_datos_iniciales[ind], string_buffer);
+						 ind ++;
               clear_token_buffer();
               k++;
           }
@@ -142,9 +206,44 @@ void on_load_file_button_file_set(){
     else{
         printf("%s\n","Error al abrir el archivo" );
       }
+
+			for (i = 0; i < 3; ++i) {
+				 printf("MAtriz[%d] = %s\n",i,  matriz_datos_iniciales[i]);
+			}
+		int i_ph = matriz_datos_iniciales[0];
+		int i_pr = matriz_datos_iniciales[1];
+		int i_number_of_games = matriz_datos_iniciales[2];
+		if(strcmp(matriz_datos_iniciales[0], "") ==0 || strcmp(matriz_datos_iniciales[1], "") ==0 || strcmp(matriz_datos_iniciales[2], "") ==0 ){
+		  create_warning_window("Los campos no pueden ser vacíos");
+		}
+		else if(!is_number(ph) || !is_number(pr) || !is_number(number_of_games)){
+		  create_warning_window("El tipo de dato debe ser entero");
+		}
+		 else{
+			 ph = atoi(i_ph);
+			 pr = atoi(i_pr);
+			 number_of_games = atoi(i_number_of_games);
+
+			 printf("ALL GOOD :) \n");
+			 ph_double = (double) ph / 100;
+			 qr_double = 1 - ph_double;
+			 pr_double = (double) pr / 100;
+			 qh_double = 1 - pr_double;
+			 printf("Mi probabilidad en casa %f\n", ph_double);
+			 printf("Probabilidad de equipo contrario on the road %f\n", qr_double);
+			 printf("Mi probabilidad on the road %f\n", pr_double);
+			 printf("Probabilidad de equipo contrario en casa %f\n", qh_double);
+			 printf("JUEGOS TOTALES %d\n", number_of_games);
+			 global_numberofgames = (int) calloc(number_of_games+1, sizeof(int));
+			defining_games_quantity = (number_of_games + 1) / 2;
+			printf("DEFIING GAMES QUANTITY = %d\n", defining_games_quantity );
+			for(k = 0; k < number_of_games; k++) global_numberofgames[k] = 0;
+			 create_buttons_window();
+		 }
 }
 
 void on_create_button_clicked(){
+	int k;
   ph = atoi(gtk_entry_get_text (ph_entry));
   pr = atoi(gtk_entry_get_text (pr_entry));
   number_of_games = atoi(gtk_entry_get_text (number_games));
@@ -168,9 +267,11 @@ void on_create_button_clicked(){
     printf("Mi probabilidad on the road %f\n", pr_double);
     printf("Probabilidad de equipo contrario en casa %f\n", qh_double);
     printf("JUEGOS TOTALES %d\n", number_of_games);
-    games_quantity = (number_of_games + 1) /2;
-    button_quantity = games_quantity +2;
-    create_entry_window();
+    global_numberofgames = (int) calloc(number_of_games+1, sizeof(int));
+		defining_games_quantity = (number_of_games + 1) / 2;
+		printf("DEFIING GAMES QUANTITY = %d\n", defining_games_quantity );
+		for(k = 0; k < number_of_games; k++) global_numberofgames[k] = 0;
+    create_buttons_window();
  }
 }
 
